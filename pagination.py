21cd9c46e917d39d429e7b395dc80233f3dc871e
马国栋 <27216650+Eric54920@users.noreply.github.com>
2020-03-31 16:32:56 +0800

    Update pagination.py

--- pagination.py
+++ pagination.py
@@ -1,33 +1,3 @@
-"""
-分页组件应用：
-1. 在视图函数中
-    queryset = models.Issues.objects.filter(project_id=project_id)
-    page_object = Pagination(
-        current_page=request.GET.get('page'),
-        all_count=queryset.count(),
-        base_url=request.path_info,
-        query_params=request.GET
-    )
-    issues_object_list = queryset[page_object.start:page_object.end]
-
-    context = {
-        'issues_object_list': issues_object_list,
-        'page_html': page_object.page_html()
-    }
-    return render(request, 'issues.html', context)
-2. 前端
-    {% for item in issues_object_list %}
-        {{item.xxx}}
-    {% endfor %}
-
-     <nav aria-label="...">
-        <ul class="pagination" style="margin-top: 0;">
-            {{ page_html|safe }}
-        </ul>
-    </nav>
-"""
-
-
 class Pagination(object):
     def __init__(self, current_page, all_count, base_url, query_params, per_page=30, pager_page_count=11):
         """
